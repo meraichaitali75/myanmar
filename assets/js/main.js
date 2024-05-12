c4c84c0d03d8c9ead80e53f32d069517d8b62f40
meraichaitali75 <meraichaitali75@gmail.com>
2024-05-11 20:50:57 -0400

    all the pages and UI designs created

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+
+// Draggable
+// $(function () {
+//     $(".draggablePins").draggable();
+//     $("#droppable").droppable({
+//         drop: function (event, ui) {
+//             $(this)
+//                 .addClass("ui-state-highlight")
+//                 .find("droppableArea")
+//                 .html("Dropped!");
+//         }
+//     });
+// });
+
+$(function () {
+    $(".draggablePins").draggable({
+        containment: "#draggableArea",
+        // When the user starts dragging, make sure the z-index for this
+        // helper is higher than all other draggables.
+        start: function (e, ui) {
+            $(".ui-draggable").not(ui.helper.css("z-index", "1"))
+                .css("z-index", "0");
+        }
+    });
+    $("#droppable").droppable({
+        drop: function (event, ui) {
+            $(this)
+                .addClass("ui-state-highlight");
+        }
+    });
+
+});
+
+jQuery("#archieve-slider").owlCarousel({
+    autoplay: true,
+    lazyLoad: true,
+    loop: true,
+    dots: false,
+    nav: false,
+    margin: 20,
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    responsive: {
+        0: {
+            items: 1
+        },
+
+        600: {
+            items: 4
+        }
+    }
+});
+
+function organize() {
+    document.getElementById("pinSelectedBtns").style.display = "flex";
+    document.getElementById("pinSelectedBtns").style.opacity = "1";
+}
---
+++ assets/js/modal.js
@@ -0,0 +1,14 @@
+$('#createBoard').click(function () {
+    var chooseBoards = document.getElementById("chooseBoards");
+    var createNewBoards = document.getElementById("createNewBoards");
+
+    chooseBoards.style.display = "none";
+    createNewBoards.style.display = "block";
+});
+$('#showBoards').click(function () {
+    var chooseBoards = document.getElementById("chooseBoards");
+    var createNewBoards = document.getElementById("createNewBoards");
+
+    chooseBoards.style.display = "block";
+    createNewBoards.style.display = "none";
+});
